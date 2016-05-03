d83b10e5d66b6b284648226ea554563e092023ec
anoopmundathan <anoop.mundathan@gmail.com>
2016-05-03 07:07:13 +0100

    add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var cssmin = require('gulp-cssmin');
 var uglify = require('gulp-uglify');
 
+gulp.task('minifyHTML', function () {
+	return gulp.src('src/*.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('dist'))
+})
+
+gulp.task('minifyCSS', function () {
+	return gulp.src('src/css/*.css')
+		.pipe(cssmin())
+		.pipe(gulp.dest('dist/css'))
+})
+
 gulp.task('uglify', function (){
-	return gulp.src('js/*')
+	return gulp.src('src/js/*')
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/js'));
 })
 
-gulp.task('default',['uglify']);
+gulp.task('default',['minifyHTML', 'minifyCSS','uglify']);
